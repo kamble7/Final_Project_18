fd33c67ffb395d99f8f8cd4ab00c421290683ca4
bolade47 <117472523+bolade47@users.noreply.github.com>
2022-11-04 17:19:53 -0700

    Add files via upload

---
+++ fileops.sv
@@ -0,0 +1,35 @@
+module fileops ();
+string filename,mode,command,s;
+integer address;
+integer fd_r;
+
+initial
+begin
+if ($value$plusargs ("filename=%s", filename))
+	$display ("Input File_name : %s",filename);
+
+if ($value$plusargs ("mode=%s", mode))
+	$display ("Operating Mode : %s",mode);
+
+if (mode==="S")
+	$display ("Operating Mode ===s : %s",mode);
+if (mode==="SILENT")
+	$display ("Operating Mode ===SILENT : %s",mode);
+	
+	
+fd_r = $fopen (filename,"r");
+if (fd_r)
+	$display ("File %s is opened successfully", filename);
+else
+	$display ("File %s is NOT opened successfully", filename);
+	
+while (!$feof(fd_r))
+begin
+	$fscanf (fd_r,"%s",command);
+	$fscanf (fd_r,"%h",address);
+	//$fgets (fd_r,"%s",content);
+	$display ("command: %d, address: %h",command,address);
+end
+$fclose (fd_r);	//closing file
+end
+endmodule : fileops
