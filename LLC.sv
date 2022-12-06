3c53a86f4e70c86c39569b779c2f9beda4142ad8
KP <117433684+kamble7@users.noreply.github.com>
2022-12-06 12:39:11 -0800

    Update LLC.sv

--- LLC.sv
+++ LLC.sv
@@ -284,6 +284,7 @@ begin
 end
 endtask : snooped_invalidate_request
 
+//******************************* SNOOP READ TASK ******************************//
 task snooped_read_request(logic [ADDR_BITS-1:0] addr);
 begin
 	which_way = search_cache;
@@ -313,13 +314,14 @@ begin
 end
 endtask : snooped_read_request
 
-//snoop_write
+//******************************* SNOOP WRITE TASK ******************************//
 task snooped_write_request (logic [ADDR_BITS-1:0] addr);
 begin
 	// Nothing to be DONE
 end
 endtask
 
+//******************************* SNOOP RWIM - Read with intent to modify TASK ******************************//
 task snooped_read_with_intent_to_modify_request(logic [ADDR_BITS-1:0] addr);
 begin
 	which_way = search_cache;
@@ -346,7 +348,8 @@ begin
 	end
 end
 endtask : snooped_read_with_intent_to_modify_request
-	
+
+//******************************* TASK TO CLEAR THE CACHE ******************************//
 task clear_cache;
 begin
 	for(int index_cnt = 0; index_cnt < SETS; index_cnt++) 
@@ -361,6 +364,7 @@ begin
 end
 endtask : clear_cache
 
+//******************************* TASK TO PRINT CONTENTS & STATES OF EACH VALID CACHE LINE ******************************//	
 task print_contents_and_state_of_each_valid_cache_line;
 begin
 int index,way;
@@ -377,9 +381,10 @@ int index,way;
 end
 endtask : print_contents_and_state_of_each_valid_cache_line
 	
-//---------------------------------------------
-//PLRU
-//---------------------------------------------
+//---------------------------------------------//
+//		       PLRU                    //
+//---------------------------------------------//
+//******************************* TASK TO UPDATE PLRU BITS ******************************//
 task UpdatePLRU (int nindex, int nway);
 	case (nway)
 		0:	begin
@@ -425,6 +430,7 @@ task UpdatePLRU (int nindex, int nway);
 	endcase
 endtask : UpdatePLRU
 
+//******************************* TASK TO GET PLRU BITS ******************************//
 function GetPLRU (int nindex);
 logic [PLRU_BITS-1:0] getlru;
 begin
@@ -466,6 +472,6 @@ begin
 end
 endfunction : GetPLRU
 
-//---------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------//
 endmodule : LLC
-//--------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------//
